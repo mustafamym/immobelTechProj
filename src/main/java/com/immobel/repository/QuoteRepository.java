d50b0ebe18260ac5746abccc24de1e014e4de1d1
Mustafa <gulam@monetizenow.io>
2022-09-21 22:49:34 +0600

    update unit test case

--- src/main/java/com/immobel/repository/QuoteRepository.java
+++ src/main/java/com/immobel/repository/QuoteRepository.java
@@ -2,6 +2,8 @@ package com.immobel.repository;
 
 import com.immobel.model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 }
--- src/test/java/com/immobel/QuoteServiceTest.java
+++ src/test/java/com/immobel/QuoteServiceTest.java
@@ -8,18 +8,18 @@ import com.immobel.service.QuoteServiceImp;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @ContextConfiguration(classes = {
-        QuoteServiceImp.class,QuoteRepository.class})
+        QuoteServiceImp.class, QuoteRepository.class, ModelMapper.class})
 public class QuoteServiceTest {
 
-    @Autowired
+    @MockBean
     QuoteRepository quoteRepository;
 
     @Autowired
